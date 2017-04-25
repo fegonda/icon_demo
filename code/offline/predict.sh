b7ed35a8c5210f85700d9b6c10a5d53e3511d31f
fegonda <felix.e.gonda@gmail.com>
2017-04-25 16:20:03 -0400

    removing files from gh-pages

--- code/offline/predict.sh
+++
@@ -1,2 +0,0 @@
-THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python predict.py
-#THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python oldpredict.py
--- code/offline/predictcnn.sh
+++
@@ -1,2 +0,0 @@
-THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python predictcnn.py
-#THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python oldpredict.py
